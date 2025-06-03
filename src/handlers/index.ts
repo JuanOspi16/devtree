120ec7b25b9679572ca3bd324cbd2765c551bbca
JuanOspina16 <juan12p16@gmail.com>
2025-06-03 15:38:08 -0500

    Validar que no existan usuarios con el mismo email

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,8 +2,18 @@ import User from "../models/User";
 import { Request, Response } from "express";
 
 export const createAccount = async (req : Request, res : Response) => {
+
+    //Validar que no exista un usuario con el mismo email
+    const {email} = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        const error = new Error("El usuario ya existe");
+        res.status(409).json({ error: error.message });
+        return;
+    }
+
     const user = new User(req.body);
     await user.save();
 
-    res.send("Registro creado correctamente");
+    res.status(201).send("Registro creado correctamente");
 };
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { createAccount } from './handlers';
+import { createAccount } from './handlers/index';
 
 const router = Router();
 
