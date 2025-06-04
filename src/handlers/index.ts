12b0a7d8b8e0d4bdebe7bf9718f96f732463ac92
JuanOspina16 <juan12p16@gmail.com>
2025-06-03 20:50:17 -0500

    Hashear contraseñas e implementación de handle

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,12 @@
 import User from "../models/User";
+import slug from "slug";
+import { hashPassword } from "../utils/auth";
 import { Request, Response } from "express";
 
 export const createAccount = async (req : Request, res : Response) => {
 
     //Validar que no exista un usuario con el mismo email
-    const {email} = req.body;
+    const {email, password} = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
         const error = new Error("El usuario ya existe");
@@ -12,7 +14,19 @@ export const createAccount = async (req : Request, res : Response) => {
         return;
     }
 
+    const handle = slug(req.body.handle, '_');
+    //Validar que no exista un usuario con el mismo handle
+    const existingHandle = await User.findOne({ handle });
+    if (existingHandle) {
+        const error = new Error("Nombre de usuario ya existe");
+        res.status(409).json({ error: error.message });
+        return;
+    }
+
+
     const user = new User(req.body);
+    user.password = await hashPassword(password);
+    user.handle = handle;
     await user.save();
 
     res.status(201).send("Registro creado correctamente");
